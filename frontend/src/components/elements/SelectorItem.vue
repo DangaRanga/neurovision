94994f1ed085f21b9ee33e25a27e82eae3826d98
Jason Gayle <jasonblue1626@gmail.com>
2022-03-13 01:55:22 -0500

    Added dataset selection component

---
+++ frontend/src/components/elements/SelectorItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <article
+    class="shadow-lg w-52 h-52 rounded-full flex items-center justify-center transition-colors duration-200"
+    :class="selected ? 'border-2 border-cyan-600' : ''"
+    id="selectable"
+    @click="toggleSelected"
+  >
+    Selection item
+  </article>
+</template>
+<script>
+export default {
+  name: "selection-item",
+  props: ["img", "selected"],
+  data() {
+    return {
+      selected: false,
+    };
+  },
+
+  methods: {
+    toggleSelected() {
+      console.log(this.selected);
+      this.selected = !this.selected;
+    },
+  },
+};
+</script>
---
+++ frontend/src/views/DatasetSelection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex">
+    <selector-item></selector-item>
+    <selector-item></selector-item>
+    <selector-item></selector-item>
+  </div>
+</template>
+<script>
+import SelectorItem from "@/components/elements/SelectorItem.vue";
+export default {
+  components: {
+    "selector-item": SelectorItem,
+  },
+};
+</script>
