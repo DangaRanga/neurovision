90ce69daf0e5e9f73e8dc8d3d56df1a9e2fb9709
Jason Gayle <jasonblue1626@gmail.com>
2022-03-13 15:13:58 -0500

    Added exit transition for checkmark

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,7 +18,16 @@ export default {
 
 <style>
 @import "./assets/styles/variables.css";
+@import "./assets/styles/animations.css";
 #selectable {
   cursor: pointer;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
