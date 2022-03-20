a723f1fb894dedc8f8e35c6119d0cafebb1837cf
Jason Gayle <jasonblue1626@gmail.com>
2022-03-20 00:48:56 -0500

    Added commands

--- backend/src/api/routes/routes.py
+++ backend/src/api/routes/routes.py
@@ -7,23 +7,27 @@ from flask import json, request, jsonify, g, make_response, abort, redirect
 # @app.route('/<path:path>')
 # def home():
 #     return redirect("/api", 200)
-    # return jsonify({"message": "This is a starter Flask Project"}), 200
+# return jsonify({"message": "This is a starter Flask Project"}), 200
 
 @app.route("/api/data", methods=["GET"])
 def get_data():
     return jsonify({"message": "This is a starter Flask Project"}), 200
 
+
 @app.route("/api/data/remove", methods=["POST"])
 def remove_feature(feature, dataframe):
     return jsonify({"message": "This is a starter Flask Project"}), 200
+
 
 @app.route("/api/data/normalize", methods=["POST"])
 def normalize():
     return jsonify({"message": "This is a starter Flask Project"}), 200
 
+
 @app.route("/api/model/fit", methods=["POST"])
 def fit(dataframe):
     return jsonify({"message": "This is a starter Flask Project"}), 200
+
 
 @app.route("/api/model/predict", methods=["POST"])
 def predict(dataframe):
