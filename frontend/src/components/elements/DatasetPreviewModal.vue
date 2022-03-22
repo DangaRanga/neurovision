6e443a151365e27101f7d608fa14754d570fb065
Jason Gayle <jasonblue1626@gmail.com>
2022-03-22 14:06:27 -0500

    Added table component and completed dataset selection screen

---
+++ frontend/src/components/elements/DatasetPreviewModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- Main modal -->
+
+  <data-table></data-table>
+</template>
+<script>
+import DataTable from "@/components/elements/DataTable.vue";
+
+export default {
+  components: {
+    "data-table": DataTable,
+  },
+
+  props: {
+    isVisible: Boolean,
+  },
+};
+</script>
