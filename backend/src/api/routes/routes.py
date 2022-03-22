837899cd9527622ea1527427a50ac6ed3531fcb2
Jantae Leckie <jleckie@altoleap.com>
2022-03-21 22:44:56 -0500

    Routes Update #1

--- backend/src/api/routes/routes.py
+++ backend/src/api/routes/routes.py
@@ -1,6 +1,12 @@
+import os
+import pandas as pd
 from api import app
 from security.jwt import decodetoken, checktoken
 from flask import json, request, jsonify, g, make_response, abort, redirect
+from werkzeug.utils import secure_filename
+from flask.helpers import send_from_directory
+from controllers.DataHandler import DataHandler
+
 
 
 # @app.route('/', defaults={'path': ''})
@@ -8,6 +14,16 @@ from flask import json, request, jsonify, g, make_response, abort, redirect
 # def home():
 #     return redirect("/api", 200)
 # return jsonify({"message": "This is a starter Flask Project"}), 200
+global datahandler
+
+@app.route("/api/create/datahandler", method=["POST"])
+def create_datahandler(fileurl):
+    url='https://drive.google.com/uc?id=' + fileurl.split('/')[-2]
+    data = pd.read_csv(url)
+    dataframe = pd.DataFrame(data)
+    datahandler = DataHandler(dataframe)
+    print(datahandler)
+    return jsonify({"message": "This is a starter Flask Project"}), 200
 
 @app.route("/api/data", methods=["GET"])
 def get_data():
