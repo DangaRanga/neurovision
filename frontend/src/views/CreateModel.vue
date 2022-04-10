06b87843c1ee47e084cfcca9eb21f42e169be805
Matthew Palmer <palmer.matthew4567@gmail.com>
2022-04-09 22:42:53 -0500

    Model Build Component

--- frontend/src/views/CreateModel.vue
+++ frontend/src/views/CreateModel.vue
@@ -1,21 +1,24 @@
 <template>
   <div>
-    <model-header :isRunning=true />
+    <!-- <model-header :isRunning=true />
     <div class="grid grid-cols-3 min-h-screen">
       <div class="bg-primary col-span-2"></div>
       <model-sidebar :isRunning=true />
-    </div>
+    </div> -->
+    <model-build />
   </div>
 </template>
 
 <script>
 import ModelHeader from "@/components/elements/ModelHeader.vue";
 import ModelSidebar from "@/components/elements/ModelSidebar.vue";
+import ModelBuild from "@/components/elements/ModelBuild.vue";
 
 export default {
   components: {
     "model-header": ModelHeader,
     "model-sidebar": ModelSidebar,
+    "model-build": ModelBuild,
   },
 };
 </script>
