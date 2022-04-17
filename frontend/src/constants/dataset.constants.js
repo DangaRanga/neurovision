81c3b26faf296a18b13c3fd58d2a711d0c6070ed
Jason Gayle <jasonblue1626@gmail.com>
2022-04-17 16:18:10 -0500

    Added constants for each dataset

---
+++ frontend/src/constants/dataset.constants.js
@@ -0,0 +1,52 @@
+import {
+  getHeartPredictionData,
+  getHousePredictionData,
+  getIrisPredictionData,
+} from "@/controllers/dataControllers/dataset.controller";
+
+const housePredictionData = {
+  title: "House Price Prediction",
+  description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+      ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+      aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur.`,
+  summary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore`,
+  problemType: "Regression",
+
+  data: getHousePredictionData(),
+};
+
+const heartDiseaseData = {
+  title: "Heart Disease",
+  description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.`,
+  summary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        eiusmod tempor incididunt ut labore`,
+  problemType: "Binary Classification",
+
+  data: getHeartPredictionData(),
+};
+
+const irisPredictionData = {
+  title: "Iris Prediction",
+  description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.`,
+  summary: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore`,
+  problemType: "Multi Classification",
+
+  data: getIrisPredictionData(),
+};
+
+export { housePredictionData, irisPredictionData, heartDiseaseData };
