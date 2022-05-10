597d045c79059bd42e445d8b6ff8a469e6b8aea8
jaleckiie22@gmail.com <jaleckiie22@gmail.com>
2022-05-10 14:01:30 -0500

    hi

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from .config import Config
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 from api.routes import routes
+
+CORS(app)
