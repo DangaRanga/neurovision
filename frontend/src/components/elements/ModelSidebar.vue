13908a8713856742f4b60e7aa751db13814fd667
perkinsashley26 <perkinsashley26@gmail.com>
2022-05-10 21:54:52 -0500

    Tour Updates #3

--- frontend/src/components/elements/ModelSidebar.vue
+++ frontend/src/components/elements/ModelSidebar.vue
@@ -99,36 +99,43 @@ export default {
         },
         {
           target: "#v-step-0",
-          content: `There are several different types of problems that can be modeled using neural networks.
-          These include: <strong>Binary Classification, Multivariate Classification, Prediction and Regression.<\strong>`,
+          content: `<strong>Click to view information on Batch Size parameters<\strong>`,
           params: {
             placement: "left",
           },
         },
         {
           target: "#v-step-0",
-          content: `<strong>Let us get some information on the Type of Analysis that the dataset is performing</strong>!`,
+          content: `<strong>Click to view information on Epochs parameter</strong>!`,
           params: {
             placement: "left",
           },
         },
         {
           target: "#v-step-0",
-          content: `<strong>Let us get some information on the Training Data Percentage needed for this dataset</strong>!`,
+          content: `<strong>Click to view information on the Learning Rate of a Neural Network </strong>!`,
           params: {
             placement: "left",
           },
         },
         {
           target: "#v-step-0",
-          content: `<strong>Let us get some information on what Normalization is </strong>.`,
+          content: `<strong>Find out what is meant by the Loss Function of a Neural Network </strong>.`,
           params: {
             placement: "left",
           },
         },
         {
           target: "#v-step-0",
-          content: `<strong>Hooray!, now that you've customized the dataset let's continue </strong>.`,
+          content: `<strong>Let's look at what is the purpose of an Optimization Algorithm </strong>.`,
+          params: {
+            placement: "left",
+          },
+        },
+        {
+          target: "#v-step-0",
+          content: `<strong>Hooray!, now that you've learnt how to customize the parameters, here comes the fun part ! 
+          <br /> <br /> Let's start the simulation and observe the changes in the Neural Network</strong> !`,
           params: {
             placement: "left",
           },
