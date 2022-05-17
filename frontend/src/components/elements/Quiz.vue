dce8fd8838165e78f3df464abdae40a7e8bccfaa
JantaeLeckie <jasonblue1626@gmail.com>
2022-05-17 11:21:13 -0500

    Streamliend styling of quiz, fixed feature engineering bug, added favicon

--- frontend/src/components/elements/Quiz.vue
+++ frontend/src/components/elements/Quiz.vue
@@ -8,7 +8,7 @@
     </h1>
 
     <section
-      class="bg-primary p-4 max-w-2xl w-full flex flex-col rounded outline outline-2 outline-white"
+      class="bg-white border py-6 px-5 max-w-2xl w-full flex flex-col rounded outline outline-2 outline-white"
       v-if="!quizCompleted"
     >
       <div class="flex flex-row justify-between mb-6">
@@ -16,7 +16,7 @@
           getCurrentQuestion.question
         }}</span>
         <div
-          class="bg-violet-400 ml-4 w-44 h-12 rounded outline outline-1 outline-white flex flex-col items-center justify-center text-base"
+          class="bg-primary_light ml-4 w-44 h-12 rounded outline outline-1 outline-white flex flex-col items-center justify-center text-base"
         >
           <span class="font-medium text-xl"
             >Score {{ score }}/{{ questions.length }}</span
@@ -30,14 +30,15 @@
           :key="index"
           :for="'option' + index"
           id="option"
-          :class="`  bg-primary_light p-4 mb-4 block rounded outline outline-1 outline-white focus:outline-grey_light 
-                    active:bg-grey_light hover:bg-grey_light h-16 text-base cursor-pointer${
-                      getCurrentQuestion.selected == index
-                        ? index == getCurrentQuestion.answer
-                          ? 'correct'
-                          : 'wrong'
-                        : ''
-                    } ${
+          class="border border-gray-300 p-4 mb-4 block rounded outline outline-1 outline-white focus:outline-grey_light active:bg-grey_light hover:bg-primary_light hover:border-blue-400 h-16 text-base hover:cursor-pointer transition-colors duration-400"
+          :class="` 
+                     ${
+                       getCurrentQuestion.selected == index
+                         ? index == getCurrentQuestion.answer
+                           ? 'border-green-400 bg-green-100'
+                           : 'border-red-400 bg-red-100'
+                         : ''
+                     } ${
             getCurrentQuestion.selected != null &&
             index != getCurrentQuestion.selected
               ? 'disabled'
@@ -54,14 +55,14 @@
             :disabled="getCurrentQuestion.selected"
             @change="SetAnswer($event)"
           />
-          <span>{{ option }}</span>
+          <span class="ml-3">{{ option }}</span>
         </label>
       </div>
 
       <button
         @click="NextQuestion()"
         :disabled="!getCurrentQuestion.selected"
-        class="justify-center w-3/5 mx-auto my-4 bg-primary_dark text-white text-lg font-medium uppercase rounded outline outline-1 outline-white appearance-none cursor-pointer p-4"
+        class="justify-center mx-auto bg-primary hover:bg-blue-500 text-white text- font-medium rounded cursor-pointer px-4 py-3 transition-colors duration-400"
       >
         {{
           getCurrentQuestion.index == questions.length - 1
@@ -189,6 +190,11 @@ export default {
 }
 
 input[type="radio"]:checked {
-  background-color: #2cce7d;
+  background-color: #78eeb3;
+}
+
+input[type="radio"] + label span:hover,
+input[type="radio"] + label:hover span {
+  transform: scale(1.2);
 }
 </style>
