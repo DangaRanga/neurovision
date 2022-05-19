c1176d65ad2f5c028afd52dcff399a557fef8217
Matthew Palmer <palmer.matthew4567@gmail.com>
2022-05-19 11:19:03 -0500

    Fixed Smax Issue along with Model Building Errors

--- backend/src/api/controllers/ModelHandler.py
+++ backend/src/api/controllers/ModelHandler.py
@@ -1,19 +1,11 @@
 import numpy as np
 import pandas as pd
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.backend import clear_session
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import SGD, Adam
 
-
 class ModelHandler(object):
-
-    problems = {
-        "heart_disease": Dense(1, "sigmoid"),
-        "house_price": Dense(1, "relu"),
-        "iris": Dense(3, "softmax"), 
-        "regression": Dense(1)
-    }
-
     """
         Things the constructor will check for in named arguments 
         1. layers  - list of nodes in hidden layer 
@@ -26,73 +18,82 @@ class ModelHandler(object):
         ?? optimizer -> SGD [Adam + others in possible development]
     """
     def __init__(self, **kwargs) -> None:
-        if "prob" not in kwargs.keys():
-          self.prob = "regression"
-        else: 
-          self.prob = kwargs["prob"]
+      if "prob" not in kwargs.keys():
+        self.prob = "regression"
+      else: 
+        self.prob = kwargs["prob"]
 
-        # Checking for the layers 
-        if "layers" not in kwargs.keys():
-          self.layers = [1]
-        else: 
-          self.layers = kwargs["layers"]
-        
-         # Checking for the activation
-        if "activations" not in kwargs.keys():
-          self.activations = ["relu"]
-        else: 
-          self.activations = kwargs["activations"]
+      # Checking for the layers 
+      if "layers" not in kwargs.keys():
+        self.layers = [1]
+      else: 
+        self.layers = kwargs["layers"]
+      
+        # Checking for the activation
+      if "activations" not in kwargs.keys():
+        self.activations = ["relu"]
+      else: 
+        self.activations = kwargs["activations"]
 
-         # Checking for the batch_size
-        if "batch_size" not in kwargs.keys():
-          self.batch_size = 1
-        else: 
-          self.batch_size = kwargs["batch_size"]
-        
-         # Checking for the epochs
-        if "epochs" not in kwargs.keys():
-          self.epochs = 10
-        else: 
-          self.epochs = kwargs["epochs"]
+        # Checking for the batch_size
+      if "batch_size" not in kwargs.keys():
+        self.batch_size = 1
+      else: 
+        self.batch_size = kwargs["batch_size"]
+      
+        # Checking for the epochs
+      if "epochs" not in kwargs.keys():
+        self.epochs = 10
+      else: 
+        self.epochs = kwargs["epochs"]
 
-         # Checking for the learning rate : lr
-        if "lr" not in kwargs.keys():
-          self.lr = 0.05
-        else: 
-          self.lr = kwargs["lr"]
+        # Checking for the learning rate : lr
+      if "lr" not in kwargs.keys():
+        self.lr = 0.05
+      else: 
+        self.lr = kwargs["lr"]
 
     def createModel(self) -> None:
-        options = []
+      options = []
+      
+      for i, num in enumerate(self.layers):
+        options.append(Dense(num, activation=self.activations[i], name="dense-{}".format(i)))
         
-        for i, num in enumerate(self.layers):
-            options.append(Dense(num, self.activations[i], name="dense-{}".format(i)))
+      if self.prob == "heart_disease":
+        options.append(Dense(1, activation="sigmoid", name="output"))
+      elif self.prob == "iris":
+        options.append(Dense(3, activation="softmax", name="output"))
+      elif self.prob == "house_price":
+        options.append(Dense(1, activation="relu", name="output"))
 
-        options.append(self.problems[self.prob])
-        self.model = Sequential(options)
+      self.model = Sequential(options)
 
-        optimizer = SGD(self.lr)
+      optimizer = SGD(self.lr)
 
-        self.model.compile(optimizer=optimizer, loss='mse', metrics=['accuracy'])
+      self.model.compile(optimizer=optimizer, loss='mse', metrics=['accuracy'])
     
     def train(self, features, output) -> dict:
-        hist = self.model.fit(features,
-                              output, 
-                              epochs=self.epochs, 
-                              batch_size=self.batch_size,
-                              verbose=0)
-        return {
-            "epochs": hist.epoch,
-            "loss_hist": hist.history['loss'],
-            "acc_hist": hist.history['accuracy']
-        }
+      hist = self.model.fit(features,
+                            output, 
+                            epochs=self.epochs, 
+                            batch_size=self.batch_size,
+                            verbose=0)
+      return {
+          "epochs": hist.epoch,
+          "loss_hist": hist.history['loss'],
+          "acc_hist": hist.history['accuracy']
+      }
 
     def evaluate(self, features, output) -> dict:
-        eval = self.model.evaluate(features, 
-                                   output)
-        return {
-            "loss": eval[0],
-            "acc": eval[1]
-        }
+      eval = self.model.evaluate(features, 
+                                  output)
+      return {
+          "loss": eval[0],
+          "acc": eval[1]
+      }
 
     def predict(self, features) -> pd.DataFrame:
-        return self.model.predict(features)
+      return self.model.predict(features)
+    
+    def clear(self):
+      clear_session()
